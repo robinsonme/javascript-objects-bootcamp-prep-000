a2ae52be1d036c6fa6e0fe2856bb59fbfc103cbd
Matthew Robinson <robinson.me@gmail.com>
2017-11-03 00:52:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artist, songTitle) {
   return playlist;
 };
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
   return playlist;
 }
